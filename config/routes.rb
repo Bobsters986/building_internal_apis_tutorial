22ad2b3252f29fc63bbfb45291ba6034b55b2b27
Bobby Luly <bobbyluly@gmail.com>
2023-03-14 10:37:06 -0600

    Pass create section

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :books, only: [:index, :show]
+      resources :books, only: [:index, :show, :create]
     end
   end
 end
