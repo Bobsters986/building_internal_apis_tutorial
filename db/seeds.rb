693b1250520f4ce99ab4e34729d0c62635f98261
Bobby Luly <bobbyluly@gmail.com>
2023-03-15 14:51:47 -0600

    Add seed data to view in postman

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+book1 = Book.create(title: "A Hobbit's Tale", author: "J.R.R. Tolkien", genre: "Fantasy", summary: "There and Back Again", number_sold: 1000000)
+book2 = Book.create(title: "The Fellowship of the Ring", author: "J.R.R. Tolkien", genre: "Fantasy", summary: "Lord of the Rings: Pt. 1", number_sold: 1200000)
+book3 = Book.create(title: "The Two Towers", author: "J.R.R. Tolkien", genre: "Fantasy", summary: "Lord of the Rings: Pt. 2", number_sold: 1300000)
+book4 = Book.create(title: "The Return of the King", author: "J.R.R. Tolkien", genre: "Fantasy", summary: "Lord of the Rings: Pt. 3", number_sold: 1400000)
